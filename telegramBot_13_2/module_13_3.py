828c49333480295661a44404236d8f8f374d764f
Druseus <79805191+DRuseus@users.noreply.github.com>
2024-10-22 22:28:47 +0300

    Выполнено задание по теме "Методы отправки сообщений".

---
+++ telegramBot_13_2/module_13_3.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher, F
+from aiogram.fsm.storage.memory import MemoryStorage
+import asyncio
+
+API = 'None'
+bot = Bot(token=API)
+dp = Dispatcher(storage=MemoryStorage())
+
+
+@dp.message(F.text == '/start')
+async def start(message):
+    await bot.send_message(chat_id=message.chat.id, text=f'Привет! Я бот, помогающий твоему здоровью.')
+
+
+@dp.message()
+async def all_messages(message):
+    await bot.send_message(chat_id=message.chat.id, text='Введите команду /start, чтобы начать общение.')
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
